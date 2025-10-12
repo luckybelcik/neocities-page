a8d38a6055f7371d0d0770b12726d27fbb33eb45
luckybelcik <luckybelcik@gmail.com>
2025-10-12 13:57:56 +0200

    fixed the soundcloud logo being lit up in blue and not orange

--- src/components/SoundcloudWidget.svelte
+++ src/components/SoundcloudWidget.svelte
@@ -3,7 +3,8 @@
         <div class="shadow-sm" style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/femtanyl" title="Femtanyl" target="_blank" style="color: #cccccc; text-decoration: none;">Femtanyl</a> · <a href="https://soundcloud.com/femtanyl/act-right" title="ACT RIGHT" target="_blank" style="color: #cccccc; text-decoration: none;">ACT RIGHT</a></div>
         <div class="inverted-color-widget fixed pointer-events-none bg-white mix-blend-difference z-[100] w-[400px] h-[125px]"></div>
         <div class="inverted-color-widget absolute pointer-events-none bg-white mix-blend-difference z-[100] left-2 bottom-[27px] w-[100px] h-[100px]"></div>
-        <div class="absolute pointer-events-none bg-[#ff0000] mix-blend-hue z-[100] left-31 bottom-[40px] w-[25px] h-[14px]"></div>
+        <div class="absolute pointer-events-none bg-[#f26e23] mix-blend-hue z-[100] left-31 bottom-[40px] w-[25px] h-[14px]"></div>
+        <div class="absolute pointer-events-none bg-[#f26e23] mix-blend-hue z-[100] left-73 bottom-[110px] w-[100px] h-[14px]"></div>
         <iframe class="bg-white" title="Soundcloud Widget" width="400" height="125" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1645522032&color=%23000000&auto_play=true&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=false"></iframe>
     </div>
 </div>
