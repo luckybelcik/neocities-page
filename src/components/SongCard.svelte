4c27171c76382a9147b8dd47f355fd0bbb781d67
luckybelcik <luckybelcik@gmail.com>
2025-10-12 13:14:58 +0200

    added a SongCard component + best tracks list

---
+++ src/components/SongCard.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import BasicInnerCard from "./BasicInnerCard.svelte";
+
+    let { title, description, iconLink, spotifyLink, appleMusicLink, youtubeLink } = $props<{ title?: string | undefined, description?: string | undefined, iconLink?: string | undefined, spotifyLink?: string | undefined, appleMusicLink?: string | undefined, youtubeLink?: string | undefined }>();
+    const displayYoutube = youtubeLink != undefined;
+    const displayAppleMusic = appleMusicLink != undefined;
+    const displaySpotify = spotifyLink != undefined;
+</script>
+
+<BasicInnerCard
+    title={title}
+    titleLink={spotifyLink}
+    description={description}
+    icon={iconLink}>
+    {#snippet top()}
+        {#if displayYoutube}
+        <a href={youtubeLink} target="_blank" aria-label="Listen on YouTube">
+            <button class="btn btn-circle btn-sm text-sm mt-1" aria-label="Listen on YouTube">
+                <svg xmlns="http://www.w3.org/2000/svg" class="fill-base-content" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
+                <path d="M 44.898438 14.5 C 44.5 12.300781 42.601563 10.699219 40.398438 10.199219 C 37.101563 9.5 31 9 24.398438 9 C 17.800781 9 11.601563 9.5 8.300781 10.199219 C 6.101563 10.699219 4.199219 12.199219 3.800781 14.5 C 3.398438 17 3 20.5 3 25 C 3 29.5 3.398438 33 3.898438 35.5 C 4.300781 37.699219 6.199219 39.300781 8.398438 39.800781 C 11.898438 40.5 17.898438 41 24.5 41 C 31.101563 41 37.101563 40.5 40.601563 39.800781 C 42.800781 39.300781 44.699219 37.800781 45.101563 35.5 C 45.5 33 46 29.398438 46.101563 25 C 45.898438 20.5 45.398438 17 44.898438 14.5 Z M 19 32 L 19 18 L 31.199219 25 Z"></path>
+                </svg>
+            </button>
+        </a>
+        {/if}
+
+        {#if displayAppleMusic}
+        <a href={appleMusicLink} target="_blank" aria-label="Listen on Apple Music">
+            <button class="btn btn-circle btn-sm text-sm mt-1" aria-label="Listen on Apple Music">
+                <svg xmlns="http://www.w3.org/2000/svg" class="fill-base-content" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
+                <path d="M37,4H13c-4.962,0-9,4.037-9,9v24c0,4.963,4.038,9,9,9h24c4.962,0,9-4.037,9-9V13C46,8.037,41.962,4,37,4z M35,27v4v0.021	h-0.002C34.986,33.768,32.749,36,30,36h-0.5c-0.987,0-1.933-0.42-2.596-1.152c-0.662-0.731-0.985-1.718-0.887-2.705	C26.195,30.38,27.787,29,29.643,29H31c1.103,0,2-0.897,2-2v-9.795l-12,2.25V30v4c0,2.757-2.243,5-5,5h-0.5	c-0.987,0-1.933-0.42-2.596-1.152c-0.662-0.731-0.985-1.718-0.887-2.705C12.195,33.38,13.787,32,15.643,32H17c1.103,0,2-0.897,2-2	V15.353c0-0.963,0.687-1.79,1.633-1.966l12.591-2.36c0.439-0.083,0.891,0.033,1.234,0.319C34.803,11.632,35,12.053,35,12.5V27z"></path>
+                </svg>
+            </button>
+        </a>
+        {/if}
+
+        {#if displaySpotify}
+        <a href={spotifyLink} target="_blank" aria-label="Listen on Spotify">
+            <button class="btn btn-circle btn-sm text-sm mt-1" aria-label="Listen on Spotify">
+                <svg xmlns="http://www.w3.org/2000/svg" class="fill-base-content" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
+                <path d="M25.009,1.982C12.322,1.982,2,12.304,2,24.991S12.322,48,25.009,48s23.009-10.321,23.009-23.009S37.696,1.982,25.009,1.982z M34.748,35.333c-0.289,0.434-0.765,0.668-1.25,0.668c-0.286,0-0.575-0.081-0.831-0.252C30.194,34.1,26,33,22.5,33.001 c-3.714,0.002-6.498,0.914-6.526,0.923c-0.784,0.266-1.635-0.162-1.897-0.948s0.163-1.636,0.949-1.897 c0.132-0.044,3.279-1.075,7.474-1.077C26,30,30.868,30.944,34.332,33.253C35.022,33.713,35.208,34.644,34.748,35.333z M37.74,29.193 c-0.325,0.522-0.886,0.809-1.459,0.809c-0.31,0-0.624-0.083-0.906-0.26c-4.484-2.794-9.092-3.385-13.062-3.35 c-4.482,0.04-8.066,0.895-8.127,0.913c-0.907,0.258-1.861-0.272-2.12-1.183c-0.259-0.913,0.272-1.862,1.184-2.12 c0.277-0.079,3.854-0.959,8.751-1c4.465-0.037,10.029,0.61,15.191,3.826C37.995,27.328,38.242,28.388,37.74,29.193z M40.725,22.013 C40.352,22.647,39.684,23,38.998,23c-0.344,0-0.692-0.089-1.011-0.275c-5.226-3.068-11.58-3.719-15.99-3.725 c-0.021,0-0.042,0-0.063,0c-5.333,0-9.44,0.938-9.481,0.948c-1.078,0.247-2.151-0.419-2.401-1.495 c-0.25-1.075,0.417-2.149,1.492-2.4C11.729,16.01,16.117,15,21.934,15c0.023,0,0.046,0,0.069,0 c4.905,0.007,12.011,0.753,18.01,4.275C40.965,19.835,41.284,21.061,40.725,22.013z"></path>
+                </svg>
+            </button>
+        </a>
+        {/if}
+    {/snippet}
+</BasicInnerCard>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
     import Shield from "../components/Shield.svelte";
     import { quotes } from "$lib/quotes";
     import DiscordCard from "../components/DiscordCard.svelte";
+    import SongCard from "../components/SongCard.svelte";
 
     function getRandomQuote(): string {
       const randomIndex = Math.floor(Math.random() * quotes.length);
@@ -67,8 +68,34 @@
                     </ul>
                 </BasicCard>
 
-                <BasicCard title="Miscellaneous" titleWeight="normal" titleSize=3 classes="bg-base-100">
+                <BasicCard title="Best Tracks" titleSize=3 classes="bg-base-100">
+                    <ul class="list gap-2">
+                    <SongCard
+                        title="Mr. Evil"
+                        description="The tale of Mr. Evil"
+                        iconLink="https://i.scdn.co/image/ab67616d0000b273e587f8b10976e615f1123a6b?format=webp&width=800&height=800"
+                        youtubeLink="https://youtu.be/n4bs0idXjdc?si=HrMHIEcF3CHPoEgg"
+                        appleMusicLink="https://music.apple.com/us/song/mr-evil-feat-my-alter-ego/1755221292"
+                        spotifyLink="https://open.spotify.com/track/1kq2Z2ahF8nJlthdKXnS7H?si=3cc12e67ed7f4041"
+                    />
 
+                    <SongCard
+                        title="EVIL RAGE BEAT OF DEATH"
+                        description="I got no clue what a rage track looks like, btw."
+                        iconLink="https://i.scdn.co/image/ab67616d0000b273b40c316bb5df8b069ad59150?format=webp&width=800&height=800"
+                        youtubeLink="https://youtu.be/gTm3slYp5uE?si=fCKBFxUvyGnkXW1g"
+                        appleMusicLink="https://music.apple.com/us/song/evil-rage-beat-of-death-feat-young-d-r-667/1827726955"
+                        spotifyLink="https://open.spotify.com/track/0D97p3VpZecLO7ulKuH4B8?si=ba3572d9b2424acd"
+                    />
+
+                    <SongCard
+                        title="exploding fetus syndrome"
+                        description="Wouldn't wanna be sick with that tbh"
+                        iconLink="https://i.scdn.co/image/ab67616d0000b2732986fe887e69614e7aa9a604?format=webp&width=800&height=800"
+                        youtubeLink="https://youtu.be/unOSF-D4Wxk?si=XJUn2RHSIzdQBtvn"
+                        spotifyLink="https://open.spotify.com/track/6HuUyZt0C0N5RDAR31ijmQ?si=f8d07153f3b0444c"
+                    />
+                    </ul>
                 </BasicCard>
             </CardRows>
         </div>
