2b72a82a731757f363929b4662193d7d95cb72e0
luckybelcik <luckybelcik@gmail.com>
2025-10-15 00:21:47 +0200

    added a new quote

--- src/lib/quotes.ts
+++ src/lib/quotes.ts
@@ -1,5 +1,8 @@
 export const quotes: string[] = [
   "jack of all trades, master of none, better than master of one",
+  
   "for you, I can become anything at all",
   "if it's to save you, I don't care how many times I die",
+  "I remember every second of a pretty memory",
+  "did I tell you that I miss you?"
 ];
