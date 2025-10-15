6fd5c111998710b65954b6d18c099b8656f916ab
luckybelcik <luckybelcik@gmail.com>
2025-10-15 20:30:50 +0200

    better button styling in RandomFactGenerator.svelte

--- src/components/RandomFactGenerator.svelte
+++ src/components/RandomFactGenerator.svelte
@@ -2,7 +2,7 @@
     const randomFacts = [
         "Tbh, I keep biting plastic. I swear I only do it because it genuinely keeps me dialed in. And it's a bit of a habit now, or maybe even an addiction. Oops!",
         "I wanna be rich give me money",
-        "The only reason I use pinterest is hot furry drawings, hood memes and MAYBE art inpiration / references like once a month."
+        "The only reason I use pinterest is hot furry drawings, hood memes, and MAYBE art inpiration / references like once a month."
     ];
 
     let lastFactIndex = 999999999;
@@ -22,10 +22,10 @@
 </script>
 
 <div class="flex flex-col justify-center mt-5 gap-5">
-    <button class="btn"
+    <button class="btn btn-success"
         onclick={() => getRandomFact()}
         tabindex="0">
-        generate fact!
+        Generate Fact!
     </button>
     <div class="bg-base-200 p-5">
         {randomFact}
